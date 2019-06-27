b8b591de912623af3a428bf52270f1c3c9849500
kaki panjang <cerita.willy@gmail.com>
2019-06-27 21:59:04 +0700

    adding simple navbar

---
+++ src/templates/src/components/NavBar.js
@@ -0,0 +1,13 @@
+import React from "react";
+
+const NavBar = () => {
+    return (
+        <nav className="navbar navbar-expand-lg navbar-light bg-light">
+            <a class="navbar-brand" href="#">
+                Navbar
+            </a>
+        </nav>
+    );
+};
+
+export default NavBar;
--- src/templates/src/index.js
+++ src/templates/src/index.js
@@ -1,15 +1,16 @@
 import React, { Component } from "react";
 import ReactDOM from "react-dom";
 
+import NavBar from "./components/NavBar";
+
 class App extends Component {
     render() {
-        return(
-            <div>
-            <h1>Hello From ReactJS!</h1>
-            <p>Its kinda hard using flask at the beginning</p>
-            </div>
-        )
+        return (
+            <React.Fragment>
+                <NavBar />
+            </React.Fragment>
+        );
     }
 }
 
-ReactDOM.render(<App />, document.getElementById("root"))
+ReactDOM.render(<App />, document.getElementById("root"));
